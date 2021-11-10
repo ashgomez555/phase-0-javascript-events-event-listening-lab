6426307cacdc959248fad77a47d2f83d82c5f76e
Ashley Gomez <ash.gomez555@gmail.com>
2021-11-10 09:25:49 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
-}
+
+
+function addEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+     alert('I was clicked!');
+});;
+   }
+
+
